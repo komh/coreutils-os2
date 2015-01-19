f00e6ab5e01aad3aea2c7eccd9d975c51c8c8c33
KO Myung-Hun <komh@chollian.net>
2015-01-19 12:01:51 +0900

    ext: gnulib: remove useless codes for sched.h
    Covered by lib/sched.in.h and m4/sched_h.m4.
    
        modified:   lib/spawn.in.h

--- lib/spawn.in.h
+++ lib/spawn.in.h
@@ -32,7 +32,7 @@
 
 /* Get definitions of 'struct sched_param' and 'sigset_t'.
    But avoid namespace pollution on glibc systems.  */
-#if !(defined __GLIBC__ && !defined __UCLIBC__) && !defined __KLIBC__
+#if !(defined __GLIBC__ && !defined __UCLIBC__)
 # include <sched.h>
 # include <signal.h>
 #endif
